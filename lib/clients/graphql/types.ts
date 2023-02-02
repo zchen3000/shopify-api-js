58979caaa73edf0ba1ba4614c9dda8b7881da3b3
Zhigang Chen <chen3000@gmail.com>
2023-02-02 08:17:37 -0800

    export GraphqlClient

--- lib/clients/graphql/types.ts
+++ lib/clients/graphql/types.ts
@@ -1,6 +1,7 @@
 import {ApiVersion} from '../../types';
 import {Session} from '../../session/session';
 import {PostRequestParams} from '../http_client/types';
+export {GraphqlClient} from './graphql_client';
 
 export type GraphqlParams = Omit<PostRequestParams, 'path' | 'type'>;
 
